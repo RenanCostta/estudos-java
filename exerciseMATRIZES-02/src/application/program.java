b32e80f6570828bfd4dc53906fed9cdc37b17e3c
Renan costa <161776441+RenanCostta@users.noreply.github.com>
2025-02-15 16:56:02 -0300

    ...

--- exerciseMATRIZES-02/src/application/program.java
+++ exerciseMATRIZES-02/src/application/program.java
@@ -16,11 +16,34 @@ public class program {
 
         int[][] mat = new int[m][n];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
                 mat[i][j] = sc.nextInt();
             }
         }
 
+        int x = sc.nextInt();
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] == x) {
+                    System.out.println("position " + i + "," + j + ":");
+                    if (j > 0) {
+                        System.out.println("left: " + mat[i][j-1]);
+                    }
+                    if (i > 0) {
+                        System.out.println("Up: " + mat[i-1][j]);
+                    }
+                    if (j < mat.length - 1) {
+                        System.out.println("Right: " + mat[i][j+1]);
+                    }
+                    if (i < mat.length - 1) {
+                        System.out.println("Down: " + mat[i+1][j]);
+                    }
+                }
+            }
+        }
+
+        sc.close();
     }
 }
