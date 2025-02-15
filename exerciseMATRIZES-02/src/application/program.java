e2f356b78373534d7f8b3dae6fee7c75cfe2d572
Renan costa <161776441+RenanCostta@users.noreply.github.com>
2025-02-14 23:16:14 -0300

    14-02-25

---
+++ exerciseMATRIZES-02/src/application/program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Linhas: ");
+        int m = sc.nextInt();
+        System.out.println("Colunas: ");
+        int n = sc.nextInt();
+
+        int[][] mat = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+    }
+}
