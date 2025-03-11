425b95d39c27e5e129c4939c0ce9abe204ae4de2
Renan costa <161776441+RenanCostta@users.noreply.github.com>
2025-03-11 15:17:46 -0300

    11-03

---
+++ exerciseHERANCA-01/src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import entities.BusinessAccount;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
+
+        BusinessAccount account = new BusinessAccount();
+
+
+
+
+
+        sc.close();
+    }
+}
