6b1896209c5fe4c9a4eda32f43256310f19332d8
Renan costa <161776441+RenanCostta@users.noreply.github.com>
2025-03-25 13:04:22 -0300

    25-03

---
+++ exerciseESTRUTURA-TRY-CATCH-01/src/application/program.java
@@ -0,0 +1,42 @@
+package application;
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+
+        method1();
+
+        System.out.println("end of program");
+
+    }
+
+    public static void method1() {
+
+        System.out.println("***METHOD 1 START");
+        method2();
+        System.out.println("***METHOD 1 END");
+
+    }
+
+    public static void method2() {
+        System.out.println("***METHOD 2 START***");
+        Scanner sc = new Scanner(System.in);
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position");
+            e.printStackTrace();
+            sc.next();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+        }
+        sc.close();
+        System.out.println("***METHOD 2 END***");
+    }
+}
